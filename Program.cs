b4e994fc536649eab6aece26a57e22658868d1d1
MariaJoseMartinezBermudez23 <martinezbermudezmariajose@gmail.com>
2026-09-25 16:36:58 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SmartHomeSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeAutomationHub smartHub = new HomeAutomationHub();
+
+
+            ISmartDevice salaLuz = new SmartLight("LIVING_ROOM_01", 75);
+            ISmartDevice oficinaAC = new SmartAirConditioner("OFFICE_AC_02", 21.5);
+
+            smartHub.RegisterDevice(salaLuz);
+            smartHub.RegisterDevice(oficinaAC);
+
+
+            smartHub.TriggerEveningRoutine();
+            smartHub.TriggerShutdownRoutine();
+
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
+
+// ENLACE JUEGO: https://dashboard.blooket.com/set/6ab6d7922eef34092c9631d4
